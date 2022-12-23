1ed564b2d1b9f7bd0a9393d718f5f801666a4083
DaitosGithub <Leokruegerhh@gmail.com>
2022-12-23 03:48:15 +0100

    just for testing

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "calculate.h"
 
 using namespace std; 
@@ -17,9 +18,10 @@ int main()
     while (true) 
     {
         cin >> x >> oper >> y;
+
         if (oper == '/' && y == 0)
         {
-             cout << "Nich Loesbar" << endl;
+            cout << "Nich Loesbar" << endl;
             continue;
         }
         else
@@ -27,18 +29,5 @@ int main()
             ergebnis = c.Calculate(x, oper, y);
         }
         cout << "Das Ergerbnis lautet: " << ergebnis << endl;
-
     }
 } 
-    
-
-// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
-
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
